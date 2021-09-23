584b036ed332643c5b5a97ac5bc6b541230a0f6f
Adam Baranec <abaranec@gmail.com>
2021-09-23 20:10:26 +0200

    some changes

--- math-gui.py
+++ math-gui.py
@@ -26,7 +26,9 @@ def prvy():
           side = int(getside)
           obsah = side * side
           zapis = str(obsah)
-          Label(okno, text="Obsah štvorca je " + zapis + ".").pack()
+          label= Label(okno, text="Obsah štvorca je " + zapis + ".")
+          label.forget_pack()
+          label.pack()
          except ValueError:
           messagebox.showerror("Pozor", "Akceptujeme len čísla.")
     if i.get() == 2:
@@ -38,7 +40,9 @@ def prvy():
           sideb = int(getb)
           obsah = sidea * sideb
           zapis = str(obsah)
-          Label(okno, text="Obsah obdĺžnika je " + zapis + ".").pack()
+          label = Label(okno, text="Obsah obdĺžnika je " + zapis + ".")
+          label.pack_forget()
+          label.pack()
          except ValueError:
           messagebox.showerror("Pozor", "Akceptujeme len čísla.")
     if i.get() == 3:
@@ -48,7 +52,9 @@ def prvy():
            radius = int(getr)
            obsah = math.pi * radius*radius
            zapis = str(obsah)
-           Label(okno, text="Obsah kruhu je " + zapis + ".").pack()
+           label = Label(okno, text="Obsah kruhu je " + zapis + ".")
+           label.pack_forget()
+           label.pack()
           except ValueError:
            messagebox.showerror("Pozor", "Akceptujeme len čísla.")
     if i.get() == 4:
@@ -60,7 +66,9 @@ def prvy():
            sideb = int(getb)
            vysledok = sidea*sidea + sideb*sideb
            zapis = str(vysledok)
-           Label(okno, text="Výsledok vety je " + zapis + ".").pack()
+           label = Label(okno, text="Výsledok vety je " + zapis + ".")
+           label.pack_forget()
+           label.pack()
           except ValueError:
            messagebox.showerror("Pozor", "Akceptujeme len čísla.")
 
@@ -73,7 +81,9 @@ def druhy():
           side = int(getside)
           obvod = side * 4
           zapis = str(obvod)
-          Label(okno, text="Obvod štvorca je " + zapis + ".").pack()
+          label = Label(okno, text="Obvod štvorca je " + zapis + ".")
+          label.pack_forget()
+          label.pack()
         except ValueError:
           messagebox.showerror("Pozor", "Akceptujeme len čísla.")
     if i.get() == 2:
@@ -85,7 +95,9 @@ def druhy():
           sideb = int(getb)
           obvod = 2 * (sidea + sideb)
           zapis = str(obvod)
-          Label(okno, text="Obvod obdĺžnika je " + zapis + ".").pack()
+          label = Label(okno, text="Obvod obdĺžnika je " + zapis + ".")
+          label.pack_forget()
+          label.pack()
          except ValueError:
           messagebox.showerror("Pozor", "Akceptujeme len čísla.")
     if i.get() == 3:
@@ -95,7 +107,9 @@ def druhy():
           radius = int(getr)
           obvod = 2 * math.pi * radius
           zapis = str(obvod)
-          Label(okno, text="Obvod kruhu je " + zapis + ".").pack()
+          label = Label(okno, text="Obvod kruhu je " + zapis + ".")
+          label.pack_forget()
+          label.pack()
          except ValueError:
           messagebox.showerror("Pozor", "Akceptujeme len čísla.")
     if i.get() == 4:
@@ -108,7 +122,9 @@ def druhy():
           vypocet = sidea*sidea + sideb*sideb
           vysledok = math.sqrt(vypocet)
           zapis = str(vysledok)
-          Label(okno, text="Dĺžka odvesny je " + zapis + ".").pack()
+          label = Label(okno, text="Dĺžka odvesny je " + zapis + ".")
+          label.pack_forget()
+          label.pack()
          except ValueError:               
           messagebox.showerror("Pozor", "Akceptujeme len čísla.")
 
@@ -116,29 +132,44 @@ obsah = Button(text="Obsah", padx=50, pady=10, command=prvy)
 obvod = Button(text="Obvod", padx=50, pady=10, command=druhy)
 vysledok = Button(text="Výsledok", padx=50, pady=10, command=prvy)
 realna_dlzka = Button(text="Reálna dĺžka", padx=50, pady=10, command=druhy)
+l1 = Label(text="Dĺžka strany:")
+l2 = Label(text="a:")
+l3 = Label(text="b:")
+l4 = Label(text="Polomer:")
+
+def hide():
+  #if someone clicked another radiobutton, the elements should not stay there
+  a.pack_forget()
+  b.pack_forget()
+  r.pack_forget()
+  obsah.pack_forget()
+  obvod.pack_forget()
+  vysledok.pack_forget()
+  realna_dlzka.pack_forget()
 
 def prepare():
-    if i.get() == 1:
-       Label(text="Dĺžka strany:").pack()
+  hide()
+  if i.get() == 1:
+       l1.pack()
        dlzka.pack()
        obsah.pack()
        obvod.pack()
-    if i.get() == 2:
-       Label(text="a:").pack()
+  if i.get() == 2:
+       l2.pack()
        a.pack()
-       Label(text="b:").pack()
+       l3.pack()
        b.pack()
        obsah.pack()
        obvod.pack()
-    if i.get() == 3:
+  if i.get() == 3:
        Label(text="Polomer:").pack()
        r.pack()
        obsah.pack()
        obvod.pack()
-    if i.get() == 4:
-       Label(text="a:").pack()
+  if i.get() == 4:
+       l2.pack()
        a.pack()
-       Label(text="b:").pack()
+       l3.pack()
        b.pack()
        vysledok.pack()
        realna_dlzka.pack()
